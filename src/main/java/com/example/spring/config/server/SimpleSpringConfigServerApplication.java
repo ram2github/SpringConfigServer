5f1970fd0ccd8d0c8d23d6dbeb34bafa35ea0828
Ramgopal Swaminathan <RamgopaS@amdocs.com>
2017-10-17 23:20:49 -0400

    Simple Config Server which connects to local gitblit server

---
+++ src/main/java/com/example/spring/config/server/SimpleSpringConfigServerApplication.java
@@ -0,0 +1,14 @@
+package com.example.spring.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SimpleSpringConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringConfigServerApplication.class, args);
+	}
+}
